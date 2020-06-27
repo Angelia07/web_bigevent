58795e488a058ed3fe6e22360c52be81730f2119
angelia <1940991165@qq.com>
2020-06-27 15:53:42 +0800

    新建一个baseAPI的js 进行url的地址拼接

---
+++ assets/js/login.js
@@ -0,0 +1,81 @@
+$(function () {
+  // 点击去注册的链接
+  $('#link_reg').on('click', function () {
+    $('.login_box').hide();
+    $('.reg_box').show();
+  })
+  // 点击去登录的链接
+  $('#link_login').on('click', function () {
+    $('.reg_box').hide();
+    $('.login_box').show();
+  })
+  // 通过layui获取from
+  //自定义layui的验证规则
+  const form = layui.form;
+  const layer = layui.layer;
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
+    repwd: function (value) {
+      const pwd = $(".reg_box [name=password]").val();
+      if (pwd !== value) {
+        return '密码不一致'
+      }
+
+    }
+  })
+  // 监听form表单发起注册 的请求
+  $('#form_reg').on('submit', function (e) {
+    e.preventDefault();//一定要阻止默认样式
+    const username = $('#form_reg [name=username]').val();
+    const password = $('#form_reg [name=password]').val();
+    $.ajax({
+      url: 'http://127.0.0.1:3007/api/register',
+      type: 'post',
+      data: {
+        username,
+        password
+      },
+      // dataType:'json',
+      success: function (res) {
+        // console.log(res);
+        if (res.status === 0) {
+
+          layer.msg(res.message);
+          $('#link_login').click();//会自己触发
+        } else {
+          layer.msg(res.message);
+        }
+      }
+
+    })
+  })
+  //监听发起登录的请求
+  $('#form_login').on('submit', function (e) {
+    e.preventDefault();
+    
+    $.ajax({
+      url: 'http://127.0.0.1:3007/api/login',
+      type: 'post',
+      data: $(this).serialize(),
+      // dataType:'json',
+      success:function(res){
+        console.log(res);
+        if(res.status===0){
+          layer.msg(res.message);
+          //进行跳转
+          // location.href='./index.html'
+          //存储token
+          localStorage.setItem('token',res.token)
+        }else(
+          layer.msg(res.message)
+        )
+
+      }
+
+    })
+
+  })
+})
